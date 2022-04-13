192c88193db965957a9fb3f27f774418e4813cba
TDCIAN <tdcian71@gmail.com>
2022-04-13 11:25:33 +0900

    first commit

---
+++ TipCalculatorUnitTests/TipCalculatorUnitTests.swift
@@ -0,0 +1,20 @@
+//
+//  TipCalculatorUnitTests.swift
+//  TipCalculatorUnitTests
+//
+//  Created by Mohammad Azam on 9/30/21.
+//
+
+import XCTest
+
+class TipCalculatorUnitTests: XCTestCase {
+
+    func test_add_two_numbers() {
+        
+        let result = 3 + 5
+        
+        XCTAssertEqual(result, 8)
+        
+    }
+
+}
